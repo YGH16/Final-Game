d4f7597e6bf34c11ca287f1574211cde698d4e6d
Muhammad Imtiyaaz Mahmood <ec18107@qmul.ac.uk>
2020-11-26 21:28:44 +0000

    animation for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,55 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	private Rigidbody2D rb2d;
+	private Vector2 movement; 
+	private float lastDirection;
+	public Animator animator; 
 	public float speed;
 	
 	void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D>();
 	}
-	
+	void update(){
+		
+		
+	}
 	void FixedUpdate()
 	{
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
+		float moveHorizontal = Input.GetAxisRaw("Horizontal");
+		float moveVertical = Input.GetAxisRaw("Vertical");
 		
-		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		movement = new Vector2(moveHorizontal, moveVertical);
+	
+		if (movement.x < 0.1 && movement.x > -0.1  && movement.y > 0.1){ // north 
+			lastDirection = 90;
+		}
+		else if (movement.x < 0.1 && movement.x > -0.1  && movement.y < -0.1){ // south
+			lastDirection = 270;
+		}
+		else if (movement.x > 0.1 && movement.y < 0.1 && movement.y > -0.1){ // east
+			lastDirection = 0;
+		}
+		else if (movement.x < -0.1 && movement.y < 0.1 && movement.y > -0.1){ // west
+			lastDirection = 180;
+		}
+		else if (movement.x > 0.1 && movement.y > 0.1){ // north east
+			lastDirection = 45;
+		}
+		else if (movement.x > 0.1 && movement.y < -0.1){ // south east
+			lastDirection = 315;
+		}
+		else if (movement.x < -0.1 && movement.y > 0.1){ //  north west
+			lastDirection = 135;
+		}
+		else if (movement.x < -0.1 && movement.y < -0.1){ // south west
+			lastDirection = 225;
+		}
 		
-		rb2d.AddForce(movement * speed);
-		
-		
+	
+		animator.SetFloat(name: "speed", value: Mathf.Max(Mathf.Abs(movement.x), Mathf.Abs(movement.y)));
+		transform.rotation = Quaternion.Euler(0,0,lastDirection);
+		rb2d.AddForce(movement * speed);		
 	}
 }
 
