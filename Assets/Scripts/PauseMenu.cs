b4d74931e1738af55ee9a7bed150429812e62a75
YoungHotBrown <sobhanakhtar16@gmail.com>
2020-12-03 18:06:47 +0000

    Pause Menu on mission 1 using esc key

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public GameObject menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        menu.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    public void Pause()
+    {
+        menu.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
